aca13adcc33bd46f0bf909e3c1c2dccf3289e9c1
Brian Yamasaki <brian.yamasaki@gmail.com>
2020-09-09 13:13:40 -0700

    Turn Control updated

--- src/components/game/TurnControl/TurnControl.tsx
+++ src/components/game/TurnControl/TurnControl.tsx
@@ -4,7 +4,13 @@ import './TurnControl.scss';
 
 const TurnControl = () => (
   <div className="turn-control">
-    <div>Turn Control</div>
+    <div className="phase">
+      <span className="active">1</span>
+      <span>2</span>
+      <span>3</span>
+      <span>4</span>
+    </div>
+    <button type="button">Done</button>
   </div>
 );
 
